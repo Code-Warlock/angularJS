c1e9b2916d1a8a0bccbb7ec0027ffaa6afdd2a24
Code-Warlock <dessymichaeltgp123@gmail.com>
2022-11-23 15:05:50 +0100

    AngularJS Routing

--- assets/js/miscbox.js
+++ assets/js/miscbox.js
@@ -1,4 +1,31 @@
-var app = angular.module("MiscApp" , []);
-app.controller("MiscController" , function($scope){
-  $scope.links = ["Home" , "About" , "Contact" , "Products" , "Login" , "Register"]
+var app = angular.module("MiscApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/home", {
+      templateUrl: "views/jumbotron.html",
+    })
+    .when("/about", {
+      templateUrl: "views/about.html",
+    })
+    .when("/contact", {
+      templateUrl: "views/contact.html",
+    }).otherwise({
+      redirectTo : '/home'
+    });
+});
+
+app.controller("MiscController", function ($scope) {
+  $scope.links = ["Home", "About", "Contact", "Products", "Login", "Register"];
+  $scope.addlink = function (link) {
+    $scope.links.push(link);
+    document.forms.data.reset();
+  };
+  $scope.dropdown = function () {
+    if ($scope.class == "remove") {
+      $scope.class = "";
+    } else {
+      $scope.class = "remove";
+    }
+  };
 });
--- assets/js/util.js
+++ assets/js/util.js
@@ -8,6 +8,12 @@ app.controller('MyController' , function($scope){
     {name:'Michael',color:'blue',networth:5000},
     {name:'Tomiwa',color:'green',networth:560000000000000000000n}
   ]
+  $scope.calculate = function (){
+    $scope.amount = ($scope.price * $scope.quantity) - (($scope.price * $scope.quantity)* ($scope.discount / 100)) 
+  }
+  // $scope.calculate = function (price , quantity , discount){
+  //   $scope.amount = (price * quantity) - ((price * quantity)* (discount / 100)) 
+  // }
 });
 // app.controller('MyController2' , function($scope){
 //   $scope.name = "John";
