835642e60d65e97d6bc9d1ffd955242f9c8a0eb8
Code-Warlock <dessymichaeltgp123@gmail.com>
2022-12-09 13:58:41 +0100

    $http object in AngularJS

--- creativeGH/js/creative-gh-controller.js
+++ creativeGH/js/creative-gh-controller.js
@@ -1,5 +1,9 @@
 var app = angular.module('creativeGHApp' , ['ngRoute' , 'ngAnimate']);
-
-app.controller('creativeGHController', function($scope){
-
+app.controller('creativeGHController', function($scope , $http){
+  $http.get('https://jsonplaceholder.typicode.com/photos').then((response)=>{
+    console.log(response)
+    $scope.photos = response.data;
+  }).catch(function(err){
+    console.log("Something went wrong")
+  });
 });
